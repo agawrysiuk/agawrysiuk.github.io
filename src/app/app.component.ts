0452ba92823e20df815c08949b52afddeb22bce2
dlrzz <arkad.gawry@gmail.com>
2021-02-21 13:31:24 +0100

    Added badge animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  private readonly animationName = 'badge-animation';
+
+  animate(target: HTMLElement): void {
+    target.classList.add(this.animationName);
+    setTimeout(() => target.classList.remove(this.animationName), 500);
+  }
 }
