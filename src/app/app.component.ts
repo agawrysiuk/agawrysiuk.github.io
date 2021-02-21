9aab0fd3e23acb5b4725874ce5d0b07540b93071
dlrzz <arkad.gawry@gmail.com>
2021-02-21 19:58:49 +0100

    Added bubble animations and more information about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,71 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({opacity: 0}),
+            animate('1s ease-out',
+              style({opacity: 1}))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({opacity: 1}),
+            animate('1s ease-in',
+              style({opacity: 0}))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent {
 
   private readonly animationName = 'badge-animation';
-  // for mobile's touch
   private animationInProcess = false;
 
-  animate(target: HTMLElement): void {
+  @ViewChild('awsBadge') awsBadge: ElementRef;
+  @ViewChild('imageWrapper') imageWrapper: ElementRef;
+  @ViewChild('bubble') bubble: ElementRef;
+  @ViewChild('bubbleBullets') bubbleBullets: ElementRef;
+  fullBubbleSpeech: boolean = false;
+
+  animate(): void {
+    // for mobile's touch
     if (!this.animationInProcess) {
       this.animationInProcess = true;
-      target.classList.add(this.animationName);
+      this.awsBadge.nativeElement.classList.add(this.animationName);
       setTimeout(() => {
-        target.classList.remove(this.animationName);
+        this.awsBadge.nativeElement.classList.remove(this.animationName);
         this.animationInProcess = false;
       }, 500);
+    }
+  }
+
+  showFullBubbleSpeech(): void {
+    if (!this.fullBubbleSpeech) {
+      this.imageWrapper.nativeElement.style.marginLeft = '0';
+      this.awsBadge.nativeElement.style.left = '38%';
+      this.bubble.nativeElement.style.left = '55%';
+      setTimeout(() => {
+        this.bubble.nativeElement.style.width = '38%';
+        this.bubble.nativeElement.style.height = '92%';
+        this.bubble.nativeElement.style.pointer = 'default';
+        this.bubbleBullets.nativeElement.style.opacity = '0';
+        setTimeout(() => {
+          this.fullBubbleSpeech = true;
+        }, 400);
+      }, 400);
     }
   }
 }
