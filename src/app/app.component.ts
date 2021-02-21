17bd276c781705a7bf8d2593714fb7a87fda8d7c
dlrzz <arkad.gawry@gmail.com>
2021-02-21 13:48:31 +0100

    Added mobile's touch responsiveness to badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent {
 
   private readonly animationName = 'badge-animation';
+  // for mobile's touch
+  private animationInProcess = false;
 
   animate(target: HTMLElement): void {
-    target.classList.add(this.animationName);
-    setTimeout(() => target.classList.remove(this.animationName), 500);
+    if (!this.animationInProcess) {
+      this.animationInProcess = true;
+      target.classList.add(this.animationName);
+      setTimeout(() => {
+        target.classList.remove(this.animationName);
+        this.animationInProcess = false;
+      }, 500);
+    }
   }
 }
